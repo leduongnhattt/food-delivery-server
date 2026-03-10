86d8b39e8595134d56b0d71fb408c7266a4c40a0
leduongnhattt <nhatnhatnhatle@gmail.com>
2026-03-10 15:28:11 +0700

    crud foods

---
+++ src/infra/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from '@infra/prisma/prisma.service';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
+
