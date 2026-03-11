a12613ead689f0121f76bb669ad1b494bbfdff6f
leduongnhattt <nhatnhatnhatle@gmail.com>
2026-03-11 19:22:53 +0700

    update-food

--- src/modules/foods/foods.module.ts
+++ src/modules/foods/foods.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@infra/prisma/prisma.module';
+import { FoodsRepository } from '@infra/repositories/foods.repository';
 import { FoodsController } from '@modules/foods/foods.controller';
 import { FoodsService } from '@modules/foods/foods.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FoodsController],
-  providers: [FoodsService],
+  providers: [FoodsRepository, FoodsService],
 })
 export class FoodsModule {}
 
